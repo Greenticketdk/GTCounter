6ae2021aab4394046422f4f749dd30518a07ac65
Hagbarth <spjerx@gmail.com>
2015-06-16 23:46:00 +0200

    a

--- src/counter.js
+++ src/counter.js
@@ -34,11 +34,12 @@ var GT = GT || {Views: {}};
 
             this.draw = function (val) {
                 this.ctx.textAlign = 'center';
+                this.ctx.textBaseline = 'middle';
                 this.ctx.font = this.size + "px Helvetica Neue";
                 this.ctx.fillStyle = this.color;
-                this.ctx.fillText(val ,this.x, this.y);
+                this.ctx.fillText(val, this.x, this.y);
                 this.ctx.font = (this.size / 2) + "px Helvetica Neue";
-                this.ctx.fillText(this.txt ,this.x, this.y + 25);
+                this.ctx.fillText(this.txt, this.x, this.y + 25);
             };
         };
 
@@ -62,16 +63,25 @@ var GT = GT || {Views: {}};
             var minCount = (minutes / 10) * 2;
             var secCount = (seconds / 60) * 2;
             var milliCount = (milliseconds / 1000) * 2;
-
-            minuteCircle.draw(minCount);
-            secondCircle.draw(secCount);
-            milliCircle.draw(milliCount);
+            if (self.settings.showCircles) {
+                minuteCircle.draw(minCount);
+                secondCircle.draw(secCount);
+                milliCircle.draw(milliCount);
+            }
+            secondText.draw((Math.floor(seconds) < 10 ? '0'+Math.floor(seconds) : Math.floor(seconds)));    
             minuteText.draw(Math.floor(minutes));
-            secondText.draw(Math.floor(seconds));
+
         };
 
         this.start = function () {
             setInterval(reload, 0);
+            if (!self.settings.showCircles) {
+                self.ctx.textAlign = 'center';
+                self.ctx.textBaseline = 'middle';
+                self.ctx.font = 27 + "px Helvetica Neue";
+                self.ctx.fillStyle = 'black';
+                self.ctx.fillText(':dasdas', 0, 0);
+            }
         };
 
         this.settings = {
@@ -84,7 +94,9 @@ var GT = GT || {Views: {}};
             width: 300,
             height: 300,
             minText: 'min',
-            secText: 'sec'
+            secText: 'sec',
+            textColor: '#ACD762',
+            showCircles: true
         };
         this.settings = mergeOptions(this.settings, options);
         var canvas = document.createElement('canvas');
@@ -97,13 +109,14 @@ var GT = GT || {Views: {}};
 
         var counterx = (canvas.width / 3) * 1.75;
         var countery = canvas.height / 2;
-        var numberx = counterx - (canvas.width / 2);
+        var numberx = (this.settings.showCircles ? (counterx - (canvas.width / 2)) : 10);
         var area = canvas.width * canvas.height;
+        var textSize = (this.settings.showCircles ? (Math.sqrt(area) / 7) : this.settings.height);
         var minuteCircle = new CountDownCircle(this.ctx, counterx, countery, Math.sqrt(area) / 3, Math.sqrt(area) / 20, this.settings.minuteColor);
         var secondCircle = new CountDownCircle(this.ctx, counterx, countery, Math.sqrt(area) / 4.5, Math.sqrt(area) / 30, this.settings.secondColor);
         var milliCircle = new CountDownCircle(this.ctx, counterx, countery, Math.sqrt(area) / 9, Math.sqrt(area) / 60, this.settings.milliColor);
-        var minuteText = new CountdownNumber(this.ctx, numberx, countery - 40, Math.sqrt(area) / 7, this.settings.minText);
-        var secondText = new CountdownNumber(this.ctx, numberx, countery + 40, Math.sqrt(area) / 7, this.settings.secText);
+        var minuteText = new CountdownNumber(this.ctx, (this.settings.showCircles ? numberx : numberx + 30), (this.settings.showCircles ? countery - 40 : countery), textSize, this.settings.minText, this.settings.textColor);
+        var secondText = new CountdownNumber(this.ctx, (this.settings.showCircles ? numberx : numberx + 30 + textSize), (this.settings.showCircles ? countery + 40 : countery), textSize, this.settings.secText, this.settings.textColor);
         var self = this;
     };
 })(GT);
